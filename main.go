d3de185fca805b5059c9ab0985189854df970b2b
Adrian Withy <adrian@adrianwithy.com>
2015-06-28 10:52:16 -0700

    Returning JSON for host info

--- main.go
+++ main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/hosts", HostIndex)
+	router.HandleFunc("/hosts/{hostId}", HostDetail)
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
+
+func HostIndex(w http.ResponseWriter, r *http.Request) {
+	hosts := Hosts{
+		Host{Name: "Host 1"},
+		Host{Name: "Host 2"},
+	}
+	json.NewEncoder(w).Encode(hosts)
+}
+
+func HostDetail(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	hostId := vars["hostId"]
+	host := Host{Name: "Host " + hostId}
+	json.NewEncoder(w).Encode(host)
+}
+
+type Host struct {
+	Name   string
+	Status bool
+}
+
+type Hosts []Host
