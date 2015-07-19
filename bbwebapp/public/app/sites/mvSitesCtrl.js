948dbb7d832a8e7226178257fb94e6d78480c934
Adrian Withy <adrian@adrianwithy.com>
2015-07-19 12:28:28 -0700

    wiring up angular routing

---
+++ bbwebapp/public/app/sites/mvSitesCtrl.js
@@ -0,0 +1,3 @@
+angular.module('app').controller('mvSitesCtrl', function($scope) {
+    $scope.hello = "Rello!";
+});
--- bbwebapp/public/js/main.js
+++ bbwebapp/public/js/main.js
@@ -1,1 +1,22 @@
-angular.module('app', [])
+angular.module('app', ['ngRoute'])
+
+angular.module('app').config(['$httpProvider', function($httpProvider) {
+    $httpProvider.interceptors.push(['$q', 'mvIdentity', function ($q, mvIdentity) {
+        return {
+            'request': function(config) {
+                config.headers = config.headers || {};
+                if(mvIdentity.currentUser) {
+                   config.headers.Authorization = 'Bearer ' + mvIdentity.currentUser.token;
+                }
+                return config;
+            },
+            'responseError': function(response) {
+                return $q.reject(response);
+            }
+        };
+    }]);
+}]);
+
+angular.module('app').config(function($routeProvider) {
+    $routeProvider.when('/sites', {templateUrl: 'app/sites/sites.html', controller: 'mvSitesCtrl'});
+});
