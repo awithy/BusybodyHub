7d599ba298f05011b5f9e3707a6e3017a9938a9d
Adrian Withy <adrian@adrianwithy.com>
2015-07-19 12:31:53 -0700

    Renamed mvIndex to mvIndexCtrl

--- bbwebapp/public/js/mvIndexCtrl.js
+++ bbwebapp/public/js/mvIndexCtrl.js
@@ -1,3 +1,3 @@
-angular.module('app').controller('mvIndex', function($scope, mvIdentity) {
+angular.module('app').controller('mvIndexCtrl', function($scope, mvIdentity) {
     $scope.identity = mvIdentity;
 });
